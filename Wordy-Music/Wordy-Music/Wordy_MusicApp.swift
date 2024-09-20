1123f89066bb39cec9a4fd66a3aaf62256612f2f
EmG9999 <emilianomondorge@gmail.Com>
2024-09-20 15:44:23 +0200

    changement en francais

---
+++ Wordy-Music/Wordy-Music/Wordy_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordy_MusicApp.swift
+//  Wordy-Music
+//
+//  Created by Apprenant 165 on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Wordy_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
