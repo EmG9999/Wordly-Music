858270ffc818639bd4f56821587d4f2fe9e3ced1
Pat75013 <karukera92@gmail.com>
2024-10-23 14:08:28 +0200

    Updates

---
+++ Wordy-Music/g.swift
@@ -0,0 +1,18 @@
+//
+//  g.swift
+//  Wordy-Music
+//
+//  Created by Apprenant 123 on 23/10/2024.
+//
+
+import SwiftUI
+
+struct g: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    g()
+}
