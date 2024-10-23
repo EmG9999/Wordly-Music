800a71abfd0df9b61c6aaaa4b4fa5377b41a873c
sks667 <lahlouh.sophian@gmail.com>
2024-10-23 14:01:57 +0200

    Updates

---
+++ Wordy-Music/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Wordy-Music
+//
+//  Created by Apprenant 178 on 23/10/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
